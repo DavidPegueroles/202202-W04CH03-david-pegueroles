78c50fb715d17c4f5a2cbcc0d5b3ba7209e6f2c0
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-02 22:04:44 +0100

    Added dialNumber function

--- src/components/Key/Key.js
+++ src/components/Key/Key.js
@@ -3,11 +3,15 @@ import DialingContext from "../../contexts/DialingContext";
 
 const Key = () => {
   const { dialingButtons } = useContext(DialingContext);
+  const { dialNumber } = useContext(DialingContext);
 
   return dialingButtons.map((dial) => {
     return (
-      <li>
-        <button className={`key${dial === "delete" ? " big" : ""}`}>
+      <li key={dial}>
+        <button
+          className={`key${dial === "delete" ? " big" : ""}`}
+          onClick={() => dialNumber(dial)}
+        >
           {dial}
         </button>
       </li>
--- src/contexts/DialingContextProvider.js
+++ src/contexts/DialingContextProvider.js
@@ -1,22 +1,20 @@
+import { useState } from "react";
 import DialingContext from "./DialingContext";
 
 const DialingContextProvider = ({ children }) => {
-  const dialingButtons = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "0",
-    "delete",
-  ];
+  const dialingButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "delete"];
+
+  const [dialedNumbers, setDialedNumbers] = useState([]);
+
+  const dialNumber = (number) => {
+    setDialedNumbers([...dialedNumbers, number]);
+    console.log(dialedNumbers);
+  };
 
   return (
-    <DialingContext.Provider value={{ dialingButtons }}>
+    <DialingContext.Provider
+      value={{ dialingButtons, dialNumber, dialedNumbers }}
+    >
       {children}
     </DialingContext.Provider>
   );
