46c14341b0c338918baaae5fa4005ef6417178cc
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-02 19:41:35 +0100

    Added Keyboard component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Actions from "./components/Actions/Actions";
 import Info from "./components/Info/Info";
+import Keyboard from "./components/Keyboard/Keyboard";
 
 function App() {
   return (
@@ -9,41 +10,7 @@ function App() {
         <Info />
         <main className="phone">
           <div className="keyboard-container">
-            <ol className="keyboard">
-              <li>
-                <button className="key">1</button>
-              </li>
-              <li>
-                <button className="key">2</button>
-              </li>
-              <li>
-                <button className="key">3</button>
-              </li>
-              <li>
-                <button className="key">4</button>
-              </li>
-              <li>
-                <button className="key">5</button>
-              </li>
-              <li>
-                <button className="key">6</button>
-              </li>
-              <li>
-                <button className="key">7</button>
-              </li>
-              <li>
-                <button className="key">8</button>
-              </li>
-              <li>
-                <button className="key">9</button>
-              </li>
-              <li>
-                <button className="key">0</button>
-              </li>
-              <li>
-                <button className="key big">delete</button>
-              </li>
-            </ol>
+            <Keyboard />
           </div>
           <Actions />
         </main>
