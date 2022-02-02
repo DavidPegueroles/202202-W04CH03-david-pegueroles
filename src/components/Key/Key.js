c8f9d9cf661c36498c73c7b6ce6926ac6b03b89d
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-02 21:13:10 +0100

    Added function to Key to render iterated elements

--- src/components/Key/Key.js
+++ src/components/Key/Key.js
@@ -1,41 +1,18 @@
+import { useContext } from "react";
+import DialingContext from "../../contexts/DialingContext";
+
 const Key = () => {
-  return (
-    <>
+  const { dialingButtons } = useContext(DialingContext);
+
+  return dialingButtons.map((dial) => {
+    return (
       <li>
-        <button className="key">1</button>
+        <button className={`key${dial === "delete" ? " big" : ""}`}>
+          {dial}
+        </button>
       </li>
-      <li>
-        <button className="key">2</button>
-      </li>
-      <li>
-        <button className="key">3</button>
-      </li>
-      <li>
-        <button className="key">4</button>
-      </li>
-      <li>
-        <button className="key">5</button>
-      </li>
-      <li>
-        <button className="key">6</button>
-      </li>
-      <li>
-        <button className="key">7</button>
-      </li>
-      <li>
-        <button className="key">8</button>
-      </li>
-      <li>
-        <button className="key">9</button>
-      </li>
-      <li>
-        <button className="key">0</button>
-      </li>
-      <li>
-        <button className="key big">delete</button>
-      </li>
-    </>
-  );
+    );
+  });
 };
 
 export default Key;
